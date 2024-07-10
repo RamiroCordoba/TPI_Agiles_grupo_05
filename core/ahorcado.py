09b1ee3f25cc56c1f6dc61b1a021290710a7f9e1
AlexisTomas2000 <alexisjosetomas@gmail.com>
2024-07-10 20:23:01 -0300

    Add test word + letter

--- core/ahorcado.py
+++ core/ahorcado.py
@@ -1,8 +1,15 @@
 class Ahorcado:
     def __init__(self):
         self.palabra_vacia = ""
+        self.palabra_NoVacia = ""
 
     def rayas_para_palabra(self, la_palabra):
         rayas = len(la_palabra)
         self.palabra_vacia = "_ " * rayas
         return self.palabra_vacia.strip()
+    
+    def letra_pertenece_palabra(self,letra):
+        return letra in self.palabra_NoVacia
+    
+    def adivina_palabra(self,palabra):
+        return self.palabra_NoVacia == palabra
