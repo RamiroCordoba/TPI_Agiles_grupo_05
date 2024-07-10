85853d4d8c07e1d1aee4d8d88c18ca09db4e4986
Ramiro Cordoba <ramirocr27@gmail.com>
2024-07-10 20:28:13 -0300

    Refactor the tests

---
+++ test_validar_letra_y_palabra.py
@@ -0,0 +1,26 @@
+import pytest
+from core.ahorcado import *
+
+
+def test_letra_pertenece():
+    un_juego = Ahorcado()
+    un_juego.palabra_NoVacia = "caramelo"
+    assert un_juego.letra_pertenece_palabra("c") == True
+
+
+def test_letra_Nopertenece():
+    un_juego = Ahorcado()
+    un_juego.palabra_NoVacia = "caramelo"
+    assert un_juego.letra_pertenece_palabra("x") == False
+
+
+def test_palabra_pertenece():
+    un_juego = Ahorcado()
+    un_juego.palabra_NoVacia = "caramelo"
+    assert un_juego.adivina_palabra("caramelo") == True
+
+
+def test_palabra_Nopertenece():
+    un_juego = Ahorcado()
+    un_juego.palabra_NoVacia = "caramelo"
+    assert un_juego.adivina_palabra("Programar") == False
--- tests/test_palabra_oculta.py
+++ tests/test_palabra_oculta.py
@@ -15,26 +15,3 @@ def test_ocultar_palabra_de_6_letras():
 def test_ocultar_palabra_de_8_letras():
     un_juego = Ahorcado()
     assert un_juego.rayas_para_palabra("caramelo") == "_ _ _ _ _ _ _ _"
-
-
-def test_letra_pertenece():
-    un_juego = Ahorcado()
-    un_juego.palabra_NoVacia = 'caramelo'
-    assert un_juego.letra_pertenece_palabra("c") == True
-
-
-def test_letra_Nopertenece():
-    un_juego = Ahorcado()
-    un_juego.palabra_NoVacia = 'caramelo'
-    assert un_juego.letra_pertenece_palabra("x") == False
-
-
-def test_palabra_pertenece():
-    un_juego = Ahorcado()
-    un_juego.palabra_NoVacia = 'caramelo'
-    assert un_juego.adivina_palabra("caramelo") == True
-
-def test_palabra_Nopertenece():
-    un_juego = Ahorcado()
-    un_juego.palabra_NoVacia = 'caramelo'
-    assert un_juego.adivina_palabra("Programar") == False
