8c063371f0d9a730036ee981ea908171ef8c0571
Ramiro Cordoba <ramirocr27@gmail.com>
2024-07-10 19:09:05 -0300

    update palabra oculta

--- tests/test_palabra_oculta.py
+++ tests/test_palabra_oculta.py
@@ -2,6 +2,16 @@ import pytest
 from core.ahorcado import *
 
 
-def test_ocultar_palabra():
+def test_ocultar_palabra_de_5_letras():
     un_juego = Ahorcado()
     assert un_juego.rayas_para_palabra("Perro") == "_ _ _ _ _"
+
+
+def test_ocultar_palabra_de_6_letras():
+    un_juego = Ahorcado()
+    assert un_juego.rayas_para_palabra("campos") == "_ _ _ _ _ _"
+
+
+def test_ocultar_palabra_de_8_letras():
+    un_juego = Ahorcado()
+    assert un_juego.rayas_para_palabra("caramelo") == "_ _ _ _ _ _ _ _"
