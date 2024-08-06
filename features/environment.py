41a0c2f8015d734d8c2cbb09c19db67f011c40f4
AlexisTomas2000 <alexisjosetomas@gmail.com>
2024-08-05 23:27:15 -0300

    prueba de correccion ATDD

--- features/environment.py
+++ features/environment.py
@@ -4,19 +4,12 @@ from selenium.webdriver.chrome.options import Options
 import chromedriver_autoinstaller
 
 def before_all(context):
-    # Instalar automáticamente la última versión de ChromeDriver
     chromedriver_autoinstaller.install()
-
-    # Configuración de opciones para Chrome
     chrome_options = Options()
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-
-    # Configuración del servicio de ChromeDriver
-    service = Service()  # No es necesario pasar el path aquí ya que `chromedriver_autoinstaller` lo maneja
-    
-    # Crear una instancia del navegador Chrome
+    service = Service() 
     context.browser = webdriver.Chrome(service=service, options=chrome_options)
 
 def after_all(context):
